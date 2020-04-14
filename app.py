87df112695c72307e4df71b21b07fe4c1c08ac79
Tomasz Wostal <tomasz.wostal@gmail.com>
2020-04-14 21:12:44 +0200

    Fixed feed groups

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ load_dotenv(dotenv_path)
 pms5003 = PMS5003(
     device=os.environ.get("PMS5003_DEVICE"),
     baudrate=os.environ.get("PMS5003_BAUDRATE"),
-    pin_enable=os.environ.get("PMS5003_PIN_ENABLE"),
-    pin_reset=os.environ.get("PMS5003_PIN_RESET")
+    pin_enable=int(os.environ.get("PMS5003_PIN_ENABLE")),
+    pin_reset=int(os.environ.get("PMS5003_PIN_RESET"))
 )
 
 
@@ -46,15 +46,15 @@ while True:
     print('Publishing {0} to {1}.{2}.'.format(data.pm_ug_per_m3(1.0), os.environ.get("ADAFRUIT_GROUP_NAME"),
                                               os.environ.get("ADAFRUIT_GROUP_FEED_PM1")))
     client.publish(os.environ.get("ADAFRUIT_GROUP_FEED_PM1"), data.pm_ug_per_m3(1.0),
-                   os.environ.get("ADAFRUIT_GROUP_FEED_PM1"))
+                   os.environ.get("ADAFRUIT_GROUP_NAME"))
 
     print('Publishing {0} to {1}.{2}.'.format(data.pm_ug_per_m3(2.5), os.environ.get("ADAFRUIT_GROUP_NAME"),
                                               os.environ.get("ADAFRUIT_GROUP_FEED_PM2.5")))
     client.publish(os.environ.get("ADAFRUIT_GROUP_FEED_PM2.5"), data.pm_ug_per_m3(2.5),
-                   os.environ.get("ADAFRUIT_GROUP_FEED_PM2.5"))
+                   os.environ.get("ADAFRUIT_GROUP_NAME"))
 
     print('Publishing {0} to {1}.{2}.'.format(data.pm_ug_per_m3(10), os.environ.get("ADAFRUIT_GROUP_NAME"),
                                               os.environ.get("ADAFRUIT_GROUP_FEED_PM10")))
     client.publish(os.environ.get("ADAFRUIT_GROUP_FEED_PM10"), data.pm_ug_per_m3(10),
-                   os.environ.get("ADAFRUIT_GROUP_FEED_PM10"))
+                   os.environ.get("ADAFRUIT_GROUP_NAME"))
     time.sleep(5)
